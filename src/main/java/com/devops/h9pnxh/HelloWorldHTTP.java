574316e2e2b6f66fca04a4631f3785e280f8dc97
David530i <david.sandor988@gmail.com>
2025-11-25 17:55:02 +0100

    feat: include neptun code in Hello World message

--- src/main/java/com/devops/h9pnxh/HelloWorldHTTP.java
+++ src/main/java/com/devops/h9pnxh/HelloWorldHTTP.java
@@ -6,7 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldHTTP {
     @GetMapping("/")
+    // az eredeti Hello World kiegeszul egy neptunkoddal
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + " - " + name();
     }
+
+    // masik method, amit a hello() meghiv kiegeszitve a szoveget
+    private String name() {
+        return "H9PNXH";
+    }
+
 }
